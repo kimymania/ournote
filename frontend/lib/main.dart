19af7c0030b2b05ee48ebb86fc2c1ed2f335f9b5
unknown <kimymania@gmail.com>
2025-12-24 23:14:03 +0900

    Added a signup page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,6 +20,8 @@ class MyApp extends StatelessWidget {
 }
 
 class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -40,14 +44,14 @@ class _LoginPageState extends State<LoginPage> {
         child: Padding(
           padding: const EdgeInsets.all(30.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: .center,
+            crossAxisAlignment: .center,
             children: [
               Text(
                 'Welcome',
                 style: TextStyle(
                   fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
+                  fontWeight: .bold,
                   fontSize: 26,
                   color: Color(0xFF1C1C1C),
                 ),
@@ -57,7 +61,7 @@ class _LoginPageState extends State<LoginPage> {
                 'Sign In to continue',
                 style: TextStyle(
                   fontFamily: 'Poppins',
-                  fontWeight: FontWeight.normal,
+                  fontWeight: .normal,
                   fontSize: 18,
                   color: Color(0xFF1C1C1C),
                 ),
@@ -70,11 +74,137 @@ class _LoginPageState extends State<LoginPage> {
                   border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 16,),
+              SizedBox(height: 16),
               TextField(
                 controller: _passwordController,
                 decoration: InputDecoration(
                   labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 26),
+              SizedBox(
+                width: double.infinity,
+                height: 49,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) {
+                      return SignupPage();
+                    }));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF3B62FF),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: .bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 26),
+              Center(
+                  child: Text(
+                    'Forgot Password?',
+                    textAlign: .center,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0xFF87879D),
+                    ),
+                  ),
+              ),
+              SizedBox(height: 10),
+              Center(
+                child: Text(
+                  "Don't have an account? Sign Up",
+                  textAlign: .center,
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontSize: 14,
+                    color: Color(0xFF87879D),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupPage createState() => _SignupPage();
+}
+
+
+class _SignupPage extends State<SignupPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _passwordCheckController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: .center,
+            crossAxisAlignment: .center,
+            children: [
+              Text(
+                'Sign Up',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: .bold,
+                  fontSize: 26,
+                  color: Color(0xFF1C1C1C),
+                ),
+              ),
+              SizedBox(height: 6),
+              Text(
+                'Sign In to continue',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: .normal,
+                  fontSize: 18,
+                  color: Color(0xFF1C1C1C),
+                ),
+              ),
+              SizedBox(height: 26),
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 16),
+              TextField(
+                controller: _passwordCheckController,
+                decoration: InputDecoration(
+                  labelText: 'Password Check',
                   border: OutlineInputBorder(),
                 ),
                 obscureText: true,
@@ -96,59 +226,42 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ),
                   child: Text(
-                    'Login',
+                    'Sign Up and Login',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                      fontWeight: .bold,
                     ),
                   ),
                 ),
               ),
-              SizedBox(height: 26),
-              Center(
-                  child: Text(
-                    'Forgot Password?',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Color(0xFF87879D),
-                    ),
-                  ),
-              ),
-              SizedBox(height: 10),
-              Center(
-                child: Text(
-                  "Don't have an account? Sign Up",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontSize: 14,
-                    color: Color(0xFF87879D),
-                  ),
-                ),
-              )
             ],
-          ),
-        ),
-      ),
+          )
+        )
+      )
     );
   }
 }
 
+class UserDashboard extends StatefulWidget {
+  const UserDashboard({Key? key}) : super(key: key);
 
-class UserDashboard extends StatelessWidget {
+  @override
+  _UserDashboard createState() => _UserDashboard();
+}
+
+class _UserDashboard extends State<UserDashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TestPage'),
+        title: Text(
+          'Dashboard'
+        ),
       ),
-      body: Center(
-        child: Text(
-          'Test'
-        )
-      )
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      ),
     );
   }
 }
