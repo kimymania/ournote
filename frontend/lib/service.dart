79622b5e6154eddddbd28aa7f31fa09977fe4c3d
kimymania <kimymania@gmail.com>
2026-01-12 16:21:29 +0900

    Added room name editing dialog and associated backend api

--- frontend/lib/service.dart
+++ frontend/lib/service.dart
@@ -69,7 +69,11 @@ class ApiService {
     }
   }
 
-  Future<void> createNewItem(String roomID, String title, String content) async {
+  Future<void> createNewItem(
+    String roomID,
+    String title,
+    String content,
+  ) async {
     try {
       final response = await http.post(
         Uri.parse("$baseUrl/room/$roomID/item/create"),
@@ -109,7 +113,12 @@ class ApiService {
     }
   }
 
-  Future<void> updateItem(String roomID, int itemID, String title, String content) async {
+  Future<void> updateItem(
+    String roomID,
+    int itemID,
+    String title,
+    String content,
+  ) async {
     try {
       final response = await http.put(
         Uri.parse("$baseUrl/room/$roomID/item/$itemID"),
@@ -129,7 +138,9 @@ class ApiService {
 
   Future<void> deleteItem(String roomID, int itemID) async {
     try {
-      final response = await http.delete(Uri.parse("$baseUrl/room/$roomID/item/$itemID"));
+      final response = await http.delete(
+        Uri.parse("$baseUrl/room/$roomID/item/$itemID"),
+      );
 
       if (response.statusCode == 200) {
         return;
@@ -148,7 +159,9 @@ class ApiService {
     try {
       final response = await http.get(url, headers: headers);
       if (response.statusCode == 200) {
-        final RoomsList roomsList = RoomsList.fromJson(jsonDecode(response.body));
+        final RoomsList roomsList = RoomsList.fromJson(
+          jsonDecode(response.body),
+        );
         final List<Room> parsedList = roomsList.list;
         return parsedList;
       } else if (response.statusCode == 401 || response.statusCode == 403) {
@@ -187,6 +200,26 @@ class ApiService {
     }
   }
 
+  Future<void> editRoomName(String roomID, String newName, Token token) async {
+    final url = Uri.parse("$baseUrl/room/$roomID");
+    final headers = Token.getHeader(token);
+    final body = jsonEncode({"room_name": newName});
+
+    try {
+      final response = await http.put(url, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        return;
+      } else if (response.statusCode == 401 || response.statusCode == 403) {
+        redirectToLoginPage();
+        throw HttpException("Session timed out");
+      } else {
+        throw HttpException("Failed to edit name");
+      }
+    } on SocketException {
+      throw Exception("Connection failure");
+    }
+  }
+
   Future<ItemsList> fetchItemsList(String roomID, Token token) async {
     final url = Uri.parse("$baseUrl/room/$roomID");
     final headers = Token.getHeader(token);
@@ -194,7 +227,9 @@ class ApiService {
     try {
       final response = await http.get(url, headers: headers);
       if (response.statusCode == 200) {
-        final ItemsList itemsList = ItemsList.fromJson(jsonDecode(response.body));
+        final ItemsList itemsList = ItemsList.fromJson(
+          jsonDecode(response.body),
+        );
         return itemsList;
       } else if (response.statusCode == 401 || response.statusCode == 403) {
         redirectToLoginPage();
@@ -224,7 +259,9 @@ class ApiService {
         throw Exception("wrong pin number");
       } else {
         int errorCode = response.statusCode;
-        throw Exception("errorCode=$errorCode, url=$url, headers=$headers, body=$body");
+        throw Exception(
+          "errorCode=$errorCode, url=$url, headers=$headers, body=$body",
+        );
       }
     } on SocketException {
       throw Exception("Connection failure");
@@ -252,7 +289,9 @@ class ApiService {
         throw Exception("wrong password");
       } else {
         int errorCode = response.statusCode;
-        throw Exception("errorCode=$errorCode, url=$url, headers=$headers, body=$body");
+        throw Exception(
+          "errorCode=$errorCode, url=$url, headers=$headers, body=$body",
+        );
       }
     } on SocketException {
       throw Exception("Connection failure");
